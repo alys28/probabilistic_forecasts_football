022a61e61d47ab787ba02100d8c1795cee5bcf61
Aly <alyshariff2810@gmail.com>
2025-01-07 01:04:37 -0500

    plan done

---
+++ data/web_scraping.py
@@ -0,0 +1,5 @@
+'''
+Two links:
+    https://cdn.espn.com/core/nfl/schedule?xhr=1&year={YEAR}&week={WEEK_NO} where 1 <= WEEK_NO <= 18 -- Get all the games with their IDs
+    https://cdn.espn.com/core/nfl/playbyplay?xhr=1&gameId=401671830 where we get all the play-by-play information
+'''
