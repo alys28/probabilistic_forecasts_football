0e0ff7a6b9b32aef6d4e51561a9e699f282e59e4
Aly <alyshariff2810@gmail.com>
2025-11-28 18:32:02 -0500

    feat: conservative C estimator

--- evalCUPF/C_estimator.py
+++ evalCUPF/C_estimator.py
@@ -1,17 +1,29 @@
 from evalCUPF.entries import Entries
 from typing import List
+import numpy as np
 
+def C_cons(pA, pB):
+    X = pA-pB # (n_games, n_timesteps)
+    delta_bar = np.mean(X, axis = 0) # (n_timesteps,)
+    X_centered = X - delta_bar[None, :] # Broadcast to (1, n_timesteps) -> (n_games, n_timesteps)
+    # Compute kernel for each entry (i, j) = C(i, j)
+    n_games = pA.shape[0]
+    C_hat = (X_centered.T @ X_centered) / n_games    # shape (n_timesteps, n_timesteps)
+    return C_hat
 
-
-def C_kernel(game_entries, estimate):
-    pass
-
-def estimate_C(entries: Entries, estimate="conservative"):
+def estimate_C(entries: Entries, p_est: List = None):
     """
-    Outputs the covariance matrix, built from the entries of 'entries', using the given estimate. 
+    Outputs the covariance matrix, built from the entries of 'entries', using the given method to estimate ground truth probabilities. 
     Args:
-    - estimate: 'conservative' or 'buckets'
+    - p_est: ground truth probabilities if we use a different estimate from 1/4
+    Output:
+    Covariance matrix of shape (n_timesteps, n_timesteps), where n_timesteps is the 2nd axis's dimension in entries.
     """
+    pA = entries.p_A
+    pB = entries.p_B
+    if p_est is None:
+        return C_cons(pA, pB)
+    raise NotImplementedError("Currently only supporting a conservative estimate of C.")
 
 
 def estimate_buckets():
--- evalCUPF/entries.py
+++ evalCUPF/entries.py
@@ -12,8 +12,9 @@ class Entries:
         self.timestep_size = timestep_size
         self.p_B = None
         self.p_A = None
+        self.Y = None
     
-    def load_entries(self, data: pd.DataFrame, timestep: str, p_A: str, p_B: str, id_field: str = "id"):
+    def load_entries(self, data: pd.DataFrame, timestep: str, p_A: str, p_B: str, y: str = "Y", id_field: str = "id"):
         """Load entries from a DataFrame grouped by ID field.
         Assumes the input DataFrame follows the format of the provided sample,
         with each set of forecasts identified by a unique ID. Groups the data
@@ -29,15 +30,19 @@ class Entries:
         games = [group.reset_index(drop=True) for _, group in data.groupby(id_field)]
         self.p_B = np.zeros((len(games), len(self.timesteps)))
         self.p_A = np.zeros((len(games), len(self.timesteps)))
+        self.Y = np.zeros((len(games), len(self.timesteps)))
 
         for i in range(len(games)):
             df = games[i]
             timestep_col = df[p_A].values
             p_A_col = df[p_A].values
             p_B_col = df[p_B].values
+            Y_col = df[y].values
             assert len(len(timestep_col) == len(self.timesteps)), f"Make sure that all your entries in your dataframe have the same number of timesteps and match with the expected number of timesteps, based on the given timestep size: {self.timestep_size}."
             self.p_A[i, :] = p_A_col
             self.p_B[i, :] = p_B_col
+            self.Y[i, :] = Y_col
+
     
     def __getitem__(self, key: Union[tuple[int, int], int]) -> Union[tuple[np.ndarray, np.ndarray], tuple[float, float]]:
         """
@@ -45,14 +50,17 @@ class Entries:
         Tuple[i, j]: get the forecasts of game i and timestep (j * timestep_size)
         Note: Both indexing methods will return forecasts of both A and B
         """
+        assert self.A is not None, "Load entries before indexing."
         if isinstance(key, tuple):
             # Two-index tuple like (i, j) -> both teams scalars
             i, j = key
             a_val = self.p_A[i, j]
             b_val = self.p_B[i, j]
-            return (a_val, b_val)
+            y_val = self.Y[i, j]
+            return (a_val, b_val, y_val)
         else:
             # Single index [i] -> return entire forecast arrays for game i
             a_vals = self.p_A[key, :]
             b_vals = self.p_B[key, :]
-            return (a_vals, b_vals)
+            y_vals = self.Y[key, :]
+            return (a_vals, b_vals, y_vals)
